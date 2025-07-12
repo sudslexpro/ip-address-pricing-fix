989b19841cd294d2587d29dd3ec797ca9ea7cf1f
SUDEEPTA SARKAR <lexprotectortech@gmail.com>
2025-07-12 21:34:01 +0530

    feat ✨: Added new HeroSection and LandingPage components
    - Created a new HeroSection component that renders a main element containing the HeroSection text.
    - Creation of a new Landing Page component with a Hero Section and basic layout structure.

---
+++ components/pages-ui/hero/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection: React.FC = () => {
+  return (
+    <main>
+      <div>HeroSection</div>
+    </main>
+  );
+};
+
+export default HeroSection;
---
+++ components/pages-ui/landing/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "@/components/pages-ui/hero/HeroSection";
+
+const LandingPage: React.FC = () => {
+	return (
+		<main>
+			<div>
+				<HeroSection />
+			</div>
+		</main>
+	);
+};
+
+export default LandingPage;
