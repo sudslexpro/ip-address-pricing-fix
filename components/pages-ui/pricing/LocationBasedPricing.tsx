b5d48de589389c9969a309c56f788967df710d52
SUDEEPTA SARKAR <lexprotectortech@gmail.com>
2025-07-29 15:49:08 +0530

    refactor ♻️: remove unused Icon import
    - The main goal of the changes is to remove the unused Icon component import.

--- components/pages-ui/pricing/LocationBasedPricing.tsx
+++ components/pages-ui/pricing/LocationBasedPricing.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Icon from "@/components/icon/AppIcon";
 import useIPLocation from "@/hooks/useIPLocation";
 
 interface ExchangeRateData {
